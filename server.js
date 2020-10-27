af35fe16d3c304cb10d30b8f984449263f56dd28
Ashwin Sathian <ashwinsathian@Ashwins-MacBook-Air.local>
2020-10-27 13:48:05 +0530

    update

--- server.js
+++ server.js
@@ -1,64 +1,64 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const cors = require('cors');
-const logger = require('./logger');
-const config = require('config');
+const cors = require("cors");
+const logger = require("./logger");
+const config = require("config");
+const path = require("path");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 // Route dependencies
-const authRoutes = require('./routes/auth.routes');
-const bookingRoutes = require('./routes/booking.routes');
-const slotRoutes = require('./routes/slot.routes');
+const authRoutes = require("./routes/auth.routes");
+const bookingRoutes = require("./routes/booking.routes");
+const slotRoutes = require("./routes/slot.routes");
 
 // DB Connection
-mongoose.connect(config.get('MONGO_URI'), 
-    { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    })
-    .then(() => {
-        logger.info({
-            function: 'mongoose_connect',
-            message: 'DB connection successful'
-        });
-    })
-    .catch((err) => {
-        logger.error({
-            function: 'mongoose_connect',
-            message: 'DB connection failed: ' + err    
-        });
-    }
-);
-
+mongoose
+  .connect(config.get("MONGO_URI"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info({
+      function: "mongoose_connect",
+      message: "DB connection successful",
+    });
+  })
+  .catch((err) => {
+    logger.error({
+      function: "mongoose_connect",
+      message: "DB connection failed: " + err,
+    });
+  });
 
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", express.static(path.join(__dirname, "client")));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/booking', bookingRoutes);
-app.use('/api/slot', slotRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/booking", bookingRoutes);
+app.use("/api/slot", slotRoutes);
 
 // Invalid route
 app.use((req, res) => {
-    logger.error({ 
-        message: 'Route ' + req.path + ' not found' 
-    });
-    res.status(404).jsonp({ 
-        errorMessage: 'Route not found: ' + req.path 
-    });
+  logger.error({
+    message: "Route " + req.path + " not found",
+  });
+  res.status(404).jsonp({
+    errorMessage: "Route not found: " + req.path,
+  });
 });
 
 app.listen(port, () => {
-    logger.info({
-        function: 'app_start',
-        message: 'Backend server is listening on port ' + port
-    });
+  logger.info({
+    function: "app_start",
+    message: "Backend server is listening on port " + port,
+  });
 });
